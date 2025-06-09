98fce76d67bea2e4b47c2fca0ef440c4f23ecedd
Nicolas Corbet <nicolas.corbet@corbet-it.fr>
2025-06-09 15:58:24 +0200

    Prepare package

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,5 @@
 # TODO: Import your package, replace this by explicit imports of what you need
-from packagename.main import predict
+from legolas.main import predict
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,6 +15,8 @@ app.add_middleware(
 )
 
 # Endpoint for https://your-domain.com/
+
+
 @app.get("/")
 def root():
     return {
@@ -22,9 +24,11 @@ def root():
     }
 
 # Endpoint for https://your-domain.com/predict?input_one=154&input_two=199
+
+
 @app.get("/predict")
 def get_predict(input_one: float,
-            input_two: float):
+                input_two: float):
     # TODO: Do something with your input
     # i.e. feed it to your model.predict, and return the output
     # For a dummy version, just return the sum of the two inputs and the original inputs
